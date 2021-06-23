c3681db9c1114eeee4a65a4346e83b3f06408191
Nathan Zhou <zna@hotmail.com.au>
2021-06-23 15:01:20 +0800

    basic website template done

--- PRsite/app/routes.py
+++ PRsite/app/routes.py
@@ -1,8 +1,38 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
+from flask import render_template, flash, redirect
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Nathan'}
-    return render_template('index.html', title='home' user=user)
+    projects = [
+        {
+            'author': 'Nathan',
+            'projectTitle': 'Some crazy Title',
+            'projectSubtitle': 'Project 1 is about this.'
+        },
+        {
+            'author': 'Nathan, Person 2',
+            'projectTitle': 'Another Bobastic Title',
+            'projectSubtitle': 'Project 2 is about that.'
+        }
+    ]
+    return render_template('index.html', title='Home',projects=projects)
+
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/resume')
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/resume')
+def resume():
+    author = {'author': 'Nathan'}
+    return render_template('resume.html', title='Home', user=author)
+
+    

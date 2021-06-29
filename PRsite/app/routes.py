ee0c80e49f80e0753525c41ff00f78da5c9ae17f
Nathan Zhou <zna@hotmail.com.au>
2021-06-29 20:13:08 +0800

    Updated Until Basic Password Protection

--- PRsite/app/routes.py
+++ PRsite/app/routes.py
@@ -1,7 +1,11 @@
-from flask import render_template
+from flask import render_template, url_for
 from app import app
 from app.forms import LoginForm
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import current_user, login_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
@@ -20,17 +24,25 @@ def index():
     ]
     return render_template('index.html', title='Home',projects=projects)
 
-
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/resume')
+        user = User.query.first()
+        if not user.check_password(form.password.data):
+            flash('Invalid password')
+            return redirect(url_for('login'))
+        login_user(user)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('resume')
+        return redirect(next_page)
+
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/resume')
+@login_required
 def resume():
     author = {'author': 'Nathan'}
     return render_template('resume.html', title='Home', user=author)

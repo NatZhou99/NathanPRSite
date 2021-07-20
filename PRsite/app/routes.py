501b7655a84b1644e1206f8e82fd5be6bb2e0d7b
Nathan Zhou <zna@hotmail.com.au>
2021-07-20 14:48:37 +0800

    Fleshing out the Websites's Design

--- PRsite/app/routes.py
+++ PRsite/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, flash
 from app import app
 from app.forms import LoginForm
 from flask import render_template, flash, redirect, request
@@ -10,11 +10,25 @@ from werkzeug.urls import url_parse
 @app.route('/')
 @app.route('/home')
 def home():
-    
-    return render_template('home.html', name=True)
+    insert="Nathan Zhou"
+    subheading="A simple website to show off my projects"
+    active=[True, False] #[Home, Projects, Resume]
+    return render_template('home.html', insert=insert, subheading=subheading, active=active)
+
+
+@app.route('/projects')
+def projects():
+    insert="Project Listings"
+    subheading="List of all completed projects"
+    active=[False, True] #[Home, Projects, Resume]
+    return render_template('projects.html', insert=insert, subheading=subheading, active=active)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    active=[False, False]
+    insert="Password Please"
+    subheading=""
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.first()
@@ -27,13 +41,15 @@ def login():
             next_page = url_for('resume')
         return redirect(next_page)
 
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', title='Password Please',insert=insert, subheading=subheading,  form=form, active=active)
 
 
 @app.route('/resume')
 @login_required
 def resume():
-    author = {'author': 'Nathan'}
-    return render_template('resume.html', title='Home', user=author)
+    active=[False, False]
+    insert="My Resume"
+    subheading=""
+    return render_template('resume.html', title='Resume',insert=insert,subheading=subheading, active=active)
 
     
